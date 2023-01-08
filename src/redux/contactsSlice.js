0b53ad340b1939096bedd43777f89884af9256a3
Kate-Kitkat19 <katekateee1911@gmail.com>
2023-01-08 15:08:57 +0200

    start files

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,15 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const INITIAL_CONTACTS = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
-
 const contactsSlice = createSlice({
   name: 'contacts',
-  initialState: { data: INITIAL_CONTACTS },
+  initialState: { items: [], isLoading: false, error: null },
   reducers: {
     addContact(state, action) {
       state.data.push(action.payload);
@@ -25,5 +18,4 @@ const contactsSlice = createSlice({
 });
 
 export const contactsReducer = contactsSlice.reducer;
-
 export const { addContact, deleteContact } = contactsSlice.actions;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,37 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { filterReducer } from './filtersSlice';
 import { contactsReducer } from './contactsSlice';
-import storage from 'redux-persist/lib/storage';
-import {
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import persistReducer from 'redux-persist/es/persistReducer';
-
-const persistConfig = {
-  key: 'contacts',
-  storage,
-  blacklist: ['filter'],
-};
 
 export const store = configureStore({
   reducer: {
-    contacts: persistReducer(persistConfig, contactsReducer),
+    contacts: contactsReducer,
     filter: filterReducer,
   },
-
-  middleware(getDefaultMiddleware) {
-    return getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    });
-  },
 });
-
-export const persistor = persistStore(store);
